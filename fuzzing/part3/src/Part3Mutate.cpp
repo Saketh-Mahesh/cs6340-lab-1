935148c3344c18d3e9ea64e3f123f8748763bfde
Saketh-Mahesh <81051119+Saketh-Mahesh@users.noreply.github.com>
2026-02-01 05:17:15 +0000

    idk man

--- fuzzing/part3/src/Part3Mutate.cpp
+++ fuzzing/part3/src/Part3Mutate.cpp
@@ -28,8 +28,7 @@
 #include <random>
 
 static std::mt19937 gen(std::random_device{}());
-const std::string CHARSET =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const std::string SPECIAL_CHARS = "!@#$%^&*()-_=+[]{};:'\",.<>/?\\|`~\n\t\r";
 
 std::map<std::string, Campaign> to_campaign = {{"MutationA", MutationA},
                                                {"MutationB", MutationB},
@@ -59,42 +58,95 @@ int generate_random_number(int min, int max) {
   return distr(gen);
 }
 
-std::string generate_random_string() {
-  int length = generate_random_number(1, 100);
+char get_random_byte() {
+  return static_cast<char>(generate_random_number(0, 255));
+}
 
+char get_random_special_char() {
+  int index =
+      generate_random_number(0, static_cast<int>(SPECIAL_CHARS.length()) - 1);
+  return SPECIAL_CHARS[index];
+}
+
+std::string generate_random_special_string(int minLen = 1, int maxLen = 100) {
+  int length = generate_random_number(minLen, maxLen);
   std::string result;
-  for (int i = 0; i < length; ++i) {
-    result += CHARSET[generate_random_number(0, CHARSET.length() - 1)];
+  for (int i = 0; i < length; i++) {
+    result += get_random_special_char();
   }
   return result;
 }
 
-std::string remove_random_characters(std::string Origin) {
-  int randomIndex = generate_random_number(0, Origin.length() - 1);
-  int randomDelete = generate_random_number(100, std::min(1000, (int)(Origin.length() - randomIndex)));
-  Origin.erase(randomIndex, randomDelete);
+std::string generate_random_string(int minLen = 1, int maxLen = 100) {
+  int length = generate_random_number(minLen, maxLen);
+  std::string result;
+  for (int i = 0; i < length; i++) {
+    result += get_random_byte();
+  }
+  return result;
+}
+
+std::string remove_random_characters(std::string Origin, int minDel = 1,
+                                     int maxDel = 100) {
+  if (Origin.empty())
+    return Origin;
+  int index = generate_random_number(0, Origin.length() - 1);
+  int deleteLegnth = generate_random_number(
+      minDel, std::min(maxDel, static_cast<int>(Origin.length() - index)));
+  Origin.erase(index, deleteLegnth);
   return Origin;
 }
 
 void perform_random_replacement(std::string &Origin) {
-  int maxReplaceLen = std::max(1, (int)(Origin.length() / 2));
+  if (Origin.empty())
+    return;
 
   int randomIndex = generate_random_number(0, Origin.length() - 1);
-  int length = generate_random_number(1, std::min(maxReplaceLen, (int)(Origin.length() - randomIndex)));
+  int length = generate_random_number(
+      1, std::max(1, static_cast<int>(Origin.length() - randomIndex)));
 
-  std::string randomString = generate_random_string();
+  int num = generate_random_number(1, 2);
+  std::string randomString;
+  if (num == 1) {
+    randomString = generate_random_special_string(1, length + 10);
+  } else {
+    randomString = generate_random_string(1, length + 10);
+  }
+
   Origin.replace(randomIndex, length, randomString);
 }
 
 void perform_random_insertion(std::string &Origin) {
-  int randomIndex = generate_random_number(0, Origin.length() - 1);
-  std::string result = generate_random_string();
-  Origin.insert(randomIndex, result);
+  int randomIndex = generate_random_number(0, Origin.length());
+
+  int num = generate_random_number(1, 2);
+  std::string randomString;
+  if (num == 1) {
+    randomString = generate_random_special_string(1, 50);
+  } else {
+    randomString = generate_random_string(1, 50);
+  }
+  Origin.insert(randomIndex, randomString);
+}
+
+void swap_adjacent_bytes(std::string &Origin) {
+  if (Origin.length() < 2)
+    return;
+  for (int i = 0; i + 3 < Origin.length(); i += 4) {
+    std::swap(Origin[i], Origin[i + 3]);
+    std::swap(Origin[i + 1], Origin[i + 2]);
+  }
+}
+
+void keep_length_within_limit(std::string &Origin, size_t maxLength = 65000) {
+  if (Origin.length() > maxLength) {
+    Origin = Origin.substr(0, maxLength);
+  }
 }
 
 std::string mutateA(std::string Origin) {
   if (Origin.empty()) {
-    return generate_random_string();
+    return Origin;
   }
 
   int num = generate_random_number(1, 2);
@@ -105,26 +157,18 @@ std::string mutateA(std::string Origin) {
     perform_random_insertion(Origin);
   }
 
-  if (Origin.length() >= 65000) {
-    Origin = remove_random_characters(Origin);
-  }
+  keep_length_within_limit(Origin);
 
   return Origin;
 }
 
-
 std::string mutateB(std::string Origin) {
   if (Origin.empty()) {
-    return generate_random_string();
+    return Origin;
   }
+  swap_adjacent_bytes(Origin);
 
-  int randomIndex = generate_random_number(0, Origin.length() - 1);
-  std::string result = generate_random_string();
-  Origin.insert(randomIndex, result);
-
-  if (Origin.length() >= 65000) {
-    Origin = remove_random_characters(Origin);
-  }
+  keep_length_within_limit(Origin);
 
   return Origin;
 }
@@ -142,9 +186,7 @@ std::string mutateC(std::string Origin) {
     Origin = generate_random_string() + Origin;
   }
 
-  if (Origin.length() >= 65000) {
-    Origin = remove_random_characters(Origin);
-  }
+  keep_length_within_limit(Origin);
 
   return Origin;
 }
